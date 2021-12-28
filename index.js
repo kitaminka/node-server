3b0b3299fd324e27ff357d55f76dc841056b2546
Kitaminka <kit4minka@outlook.com>
2021-12-28 10:32:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require('./src/server');
 require('dotenv').config();
 
 server.start().then(() => {
-    console.log(`Server started on port ${process.env.PORT || 3000}`)
+    console.log(`Server started on port ${process.env.PORT || 3000}`);
 });
